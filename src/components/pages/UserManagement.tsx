50dc86c21747a9c0005d4b3d4397bbcd4104d936
Youuu <31610705+73-yy@users.noreply.github.com>
2022-04-30 20:52:00 +0900

    updated

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,13 +1,21 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
+import { Wrap, WrapItem } from "@chakra-ui/react";
+import { UserCard } from "../organisms/user/UserCard";
+import { UseAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
+  const { getUsers, loading, users } = UseAllUsers();
+  useEffect();
+
   return (
-    <Wrap>
-      {[...Array()]}
+    <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <div
-          style={{ width: "100%", height: "100px", backgroundColor: "teal" }}
+        {getUsers}
+        {users.map}
+        <UserCard
+          imageUrl="https://source.unsplash.com/random"
+          userName="jake"
+          fullName="jakejake"
         />
       </WrapItem>
     </Wrap>
