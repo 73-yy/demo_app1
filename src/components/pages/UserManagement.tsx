210c6b432ecd60444d1fb500139dc9ddfa18cd8e
Youuu <31610705+73-yy@users.noreply.github.com>
2022-04-30 21:18:07 +0900

    updated

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,23 +1,39 @@
-import { memo, VFC } from "react";
-import { Wrap, WrapItem } from "@chakra-ui/react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+
+import { memo, useEffect, VFC } from "react";
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
 import { UserCard } from "../organisms/user/UserCard";
 import { UseAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
   const { getUsers, loading, users } = UseAllUsers();
-  useEffect();
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
-    <Wrap p={{ base: 4, md: 10 }}>
-      <WrapItem>
-        {getUsers}
-        {users.map}
-        <UserCard
-          imageUrl="https://source.unsplash.com/random"
-          userName="jake"
-          fullName="jakejake"
-        />
-      </WrapItem>
-    </Wrap>
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map((user) => {
+            return (
+              <WrapItem key={user.id} mx="auto">
+                <UserCard
+                  imageUrl="https://source.unsplash.com/random"
+                  userName={user.username}
+                  fullName={user.name}
+                />
+              </WrapItem>
+            );
+          })}
+        </Wrap>
+      )}
+    </>
   );
 });
